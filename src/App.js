389e59b34d5135d6ce72e23e46daca3c8749f5af
sabithrajasekarR <r.sabithrajasekar@gmail.com>
2023-01-07 12:21:38 +0530

    version 1.0

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './mainfile/js files/nav';
+
+function App() {
+  return (
+    <NavBar/>
+  );
+}
+
+export default App;
---
+++ src/mainfile/js files/nav.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function NavBar(){
+
+
+
+
+
+    return(
+        <div>
+            <img src="../s-logo-vector-letter.webp"></img>
+            <input type="search" ></input>
+            <button>Search</button>
+            <i class="fa-light fa-cart-shopping"></i>
+            <nav class="navbar bg-dark" data-bs-theme="dark">
+
+</nav>
+
+<nav class="navbar bg-primary" data-bs-theme="dark">
+
+</nav>
+
+<nav class="navbar" style="background-color: #e3f2fd;">
+
+</nav>
+        </div>
+    )
+}
+
+export default NavBar
